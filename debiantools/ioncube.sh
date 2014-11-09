65059d6384c6f5833c055f065db42df59f88a3d4
qingliu <qingliu@live.cn>
2014-11-10 06:53:07 +0800

    update debian tools

--- debiantools/ioncube.sh
+++ debiantools/ioncube.sh
@@ -2,7 +2,7 @@
 
 clear
 echo "========================================================================="
-echo "ionCube Installation for LLsMP 0.6 Written by llsmp.cn"
+echo "ionCube Installation for LLsMP 1.0 Written by llsmp.cn"
 echo "========================================================================="
 echo "LLsMP is A tool to auto-compile & install Litespeed+MySQL+PHP on Linux "
 echo ""
@@ -29,33 +29,33 @@ echo "========================================================================="
 mkdir /tmp/llsmp
 cd /tmp/llsmp
 
-if [ -f /usr/local/lsws/fcgi-bin/lsphp-5.3.* ];then
+if [ -f /usr/local/lsws/fcgi-bin/lsphp-5.4.* ];then
 bit=$(getconf LONG_BIT)
 if [ "$bit" = "64" ]; then
-wget -c http://downloads2.ioncube.com/loader_downloads/ioncube_loaders_lin_x86-64.tar.gz
+wget -c http://downloads3.ioncube.com/loader_downloads/ioncube_loaders_lin_x86-64.tar.gz
 tar zxvf ioncube_loaders_lin_x86-64.tar.gz
 else
-wget -c http://downloads2.ioncube.com/loader_downloads/ioncube_loaders_lin_x86.tar.gz
+wget -c http://downloads3.ioncube.com/loader_downloads/ioncube_loaders_lin_x86.tar.gz
 tar zxvf ioncube_loaders_lin_x86.tar.gz
 fi
 
-mv ioncube/ioncube_loader_lin_5.3.so /usr/local/lsws/lsphp5/lib/php/extensions/no-debug-non-zts-20090626/ioncube_loader_lin_5.3.so
-sed -i '/\[zend\]/i\\n\[ionCube\]\nzend_extension="/usr/local/lsws/lsphp5/lib/php/extensions/no-debug-non-zts-20090626/ioncube_loader_lin_5.3.so"\n' /usr/local/lsws/lsphp5/lib/php.ini
+mv ioncube/ioncube_loader_lin_5.4.so /usr/local/lsws/lsphp5/lib/php/extensions/no-debug-non-zts-20090626/ioncube_loader_lin_5.4.so
+sed -i '/\[zend\]/i\\n\[ionCube\]\nzend_extension="/usr/local/lsws/lsphp5/lib/php/extensions/no-debug-non-zts-20090626/ioncube_loader_lin_5.4.so"\n' /usr/local/lsws/lsphp5/lib/php.ini
 /etc/init.d/lsws restart
 
 else
 
 bit=$(getconf LONG_BIT)
 if [ "$bit" = "64" ]; then
-wget -c http://downloads2.ioncube.com/loader_downloads/ioncube_loaders_lin_x86-64.tar.gz
+wget -c http://downloads3.ioncube.com/loader_downloads/ioncube_loaders_lin_x86-64.tar.gz
 tar zxvf ioncube_loaders_lin_x86-64.tar.gz
 else
-wget -c http://downloads2.ioncube.com/loader_downloads/ioncube_loaders_lin_x86.tar.gz
+wget -c http://downloads3.ioncube.com/loader_downloads/ioncube_loaders_lin_x86.tar.gz
 tar zxvf ioncube_loaders_lin_x86.tar.gz
 fi
 
-mv ioncube/ioncube_loader_lin_5.2.so /usr/local/lsws/lsphp5/lib/php/extensions/no-debug-non-zts-20060613/ioncube_loader_lin_5.2.so
-sed -i '/\[zend\]/i\\n\[ionCube\]\nzend_extension="/usr/local/lsws/lsphp5/lib/php/extensions/no-debug-non-zts-20060613/ioncube_loader_lin_5.2.so"\n' /usr/local/lsws/lsphp5/lib/php.ini
+mv ioncube/ioncube_loader_lin_5.3.so /usr/local/lsws/lsphp5/lib/php/extensions/no-debug-non-zts-20060613/ioncube_loader_lin_5.3.so
+sed -i '/\[zend\]/i\\n\[ionCube\]\nzend_extension="/usr/local/lsws/lsphp5/lib/php/extensions/no-debug-non-zts-20060613/ioncube_loader_lin_5.3.so"\n' /usr/local/lsws/lsphp5/lib/php.ini
 /etc/init.d/lsws restart
 
 fi
